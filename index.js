43c3778547b634892314cda1a2ca4566d77fc9bb
KUNALLATTA29 <kunallatta2@gmail.com>
2024-11-17 21:31:58 +0530

    update

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors');
 require('dotenv').config()
 
-
-const app = express();
-const PORT = 8080;
+const port = process.env.port || 8080
 const mongo_url = process.env.mongo_url
-
+const app = express()
+app.use(cors())
 app.use(express.json())
 
 mongoose.connect(mongo_url)
-.then(()=>{
-    console.log('connected to database')
+.then((req,res)=>{
+    console.log('database is connected')
 }).catch((err)=>{
-    console.log("ther is an error in database",err)
+    console.log('there is an error',err)
 })
 
-const salesschema = new mongoose.Schema({
+const salesSchema = new mongoose.Schema({
     productName:{
         type:String,
         required:true
@@ -28,48 +28,61 @@ const salesschema = new mongoose.Schema({
     quantity:{
         type:Number,
         required:true
+
     },
     date:{
         type:Date,
-        required:true,
-        default:Date.now
+        default:Date.now,
     }
-},{timestamps:true})
+})
 
-const sales = mongoose.model('sales',salesschema)
+const sales = mongoose.model('sales',salesSchema)
 
 app.get('/findall',async(req,res)=>{
     try{
-        const mysalesdata = await sales.find()
-        res.status(200).json(mysalesdata)
+        const salesdata = await sales.find();
+        res.status(200).json(salesdata)
     }catch(err){
-        console.log("there is error",err)
-        res.status(500).send("error in fetching")
+        res.status(500).json({message:'error in fetching',err})
     }
-    
 })
 
-app.post('/addsale',async(req,res)=>{
+app.delete('/delete/:id',async(req,res)=>{
     try{
-        const{productName,amount,quantity}=req.body;
-        if(!productName || !amount){
-            return res.status(400).json({message:'data is missing'})
+        const deleteitem = await sales.findByIdAndDelete(req.params.id);
+        if(!deleteitem){
+            return res.status(404).json({message:'data not found'})
         }
-        const newsale = new sales({
-            productName,
-            amount,
-            quantity
-        })
-        const savesale = await newsale.save();
-        res.status(201).json(savesale);
+        res.status(200).json({message:'done!'})
     }catch(err){
-        console.log('error adding sale',err)
-        res.status(500).send('error')
+        res.status(500).json({message:'error in deleting',err})
     }
 })
 
+app.post('/add',async(req,res)=>{
+    try{
+        const newsale = new sales(req.body);
+        const savedsale = await newsale.save()
+        res.status(200).json(savedsale)
+    }catch(err){
+        res.status(400).json({message:'error in saving',err})
+    }
+})
 
+app.put('/update/:id',async(req,res)=>{
+    try{
+        const updatedsale = await sales.findByIdAndUpdate(req.params.id, req.body,{
+            new:true,
+        })
+        if(!updatedsale){
+            return res.status(404).json({message:'sale not found'});
+        }
+        res.status(200).json(updatedsale);
+    }catch(err){
+        res.status(400).json({message:'error in updating', err})
+    }
+})
 
-app.listen(PORT,()=>{
-    console.log(`port is running on ${PORT}`)
+app.listen(port,()=>{
+    console.log("server is running")
 })
